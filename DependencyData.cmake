846e53d76a4ac02e69251ce74992cab572b36ccf
Chris Jaquet <chrisjaquet@gmail.com>
2015-11-14 08:16:42 +0200

    Adding preliminary CMake files

---
+++ DependencyData.cmake
@@ -0,0 +1,122 @@
+# Package version numbers
+SET(DEP_archive libarchive-2.8.4 libarchive-2.8.4.tar.gz)
+SET(DEP_boost boost-1.50 boost-1.50.tar.bz2)
+SET(DEP_cdio libcdio-0.93 libcdio-0.93.tar.gz)
+SET(DEP_chromaprint chromaprint-1.1 chromaprint-1.1.tar.gz)
+SET(DEP_dlfcn dlfcn-r19 dlfcn-win32-static-r19.tar.bz2)
+SET(DEP_echonest libechonest-2.3.0 libechonest-2.3.0.tar.gz)
+SET(DEP_faac faac-1.28 faac-1.28.tar.gz)
+SET(DEP_faad faad2-2.7 faad2-2.7.tar.gz)
+SET(DEP_ffi libffi-3.0.11 libffi-3.0.11.tar.gz)
+SET(DEP_fftw fftw-3.3.2 fftw-3.3.2.tar.gz)
+SET(DEP_flac flac-1.2.1 flac-1.2.1.tar.gz)
+SET(DEP_gcrypt libgcrypt-1.6.1 libgcrypt-1.6.1.tar.bz2)
+SET(DEP_gettext gettext-0.18.3.1 gettext-0.18.3.1.tar.gz)
+SET(DEP_glib glib-2.42.0 glib-2.42.0.tar.xz)
+SET(DEP_glibnetworking glib-networking-2.42.0 glib-networking-2.42.0.tar.xz)
+SET(DEP_gmp gmp-5.1.3 gmp-5.1.3.tar.xz)
+SET(DEP_gnutls gnutls-3.2.12 gnutls-3.2.12.tar.xz)
+SET(DEP_gpgerror libgpg-error-1.12 libgpg-error-1.12.tar.bz2)
+SET(DEP_gpod libgpod-0.8.0 libgpod-0.8.0.tar.gz)
+SET(DEP_gst gstreamer-1.4.2 gstreamer-1.4.2.tar.xz)
+SET(DEP_gstbad gst-plugins-bad-1.4.2 gst-plugins-bad-1.4.2.tar.xz)
+SET(DEP_gstbase gst-plugins-base-1.4.2 gst-plugins-base-1.4.2.tar.xz)
+SET(DEP_gstgood gst-plugins-good-1.4.2 gst-plugins-good-1.4.2.tar.xz)
+SET(DEP_gstlibav gst-libav-1.4.2 gst-libav-1.4.2.tar.xz)
+SET(DEP_gstugly gst-plugins-ugly-1.4.2 gst-plugins-ugly-1.4.2.tar.xz)
+SET(DEP_id3tag libid3tag-0.15.1b libid3tag-0.15.1b.tar.gz)
+SET(DEP_lame lame-3.98.4 lame-3.98.4.tar.gz)
+SET(DEP_lastfm davidsansome-liblastfm-6748fcf davidsansome-liblastfm-0.3.1-17-g6748fcf.tar.gz)
+SET(DEP_libiconv libiconv-1.14 libiconv-1.14.tar.gz)
+SET(DEP_mad libmad-0.15.1b libmad-0.15.1b.tar.gz)
+SET(DEP_mpcdec libmpcdec-1.2.6 libmpcdec-1.2.6.tar.bz2)
+SET(DEP_ogg libogg-1.2.0 libogg-1.2.0.tar.gz)
+SET(DEP_oil liboil-0.3.17 liboil-0.3.17.tar.gz)
+SET(DEP_opus opus-1.0.2 opus-1.0.2.tar.gz)
+SET(DEP_orc orc-0.4.16 orc-0.4.16.tar.gz)
+SET(DEP_plist libplist-1.10 libplist-1.10.tar.bz2)
+SET(DEP_protobuf protobuf-2.5.0 protobuf-2.5.0.tar.bz2)
+SET(DEP_proxy libproxy-0.4.11 libproxy-0.4.11.tar.gz)
+SET(DEP_qjson qjson-0.8.1 qjson-0.8.1.tar.gz)
+SET(DEP_qtsparkle davidsansome-qtsparkle-28721b5 davidsansome-qtsparkle-28721b5.tar.gz)
+SET(DEP_quicktime libquicktime-1.1.5 libquicktime-1.1.5.tar.gz)
+SET(DEP_sip sip-4.12.2 sip-4.12.2.tar.gz)
+SET(DEP_soup libsoup-2.48.0 libsoup-2.48.0.tar.xz)
+SET(DEP_sparsehash sparsehash-2.0.2 sparsehash-2.0.2.tar.gz)
+SET(DEP_speex speex-1.2rc1 speex-1.2rc1.tar.gz)
+SET(DEP_sqlite sqlite-3.7.0 sqlite-amalgamation-3.7.0.tar.gz)
+SET(DEP_taglib taglib-1.9.1 taglib-1.9.1.tar.gz)
+SET(DEP_tls gnutls-2.10.1 gnutls-2.10.1.zip)
+SET(DEP_vorbis libvorbis-1.3.1 libvorbis-1.3.1.tar.gz)
+SET(DEP_wavpack wavpack-4.60.1 wavpack-4.60.1.tar.bz2)
+SET(DEP_xml libxml2-2.9.1 libxml2-2.9.1.tar.gz)
+
+# Windows only packages
+SET(DEP_glew glew-1.5.5 glew-1.5.5-win32.zip)
+SET(DEP_mmswin32 libmms-win32 libmms-win32.tar.gz)
+SET(DEP_openssl openssl-1.0.1m openssl-1.0.1m.tar.gz)
+SET(DEP_pthreads pthreads-win32-2.8.0 pthreads-win32-2.8.0.tar.gz)
+SET(DEP_qt qt-everywhere-opensource-src-4.8.3 qt-everywhere-opensource-src-4.8.3.tar.gz)
+SET(DEP_spotifywin32 libspotify-12.1.45-win32-release libspotify-12.1.45-win32-release.zip)
+SET(DEP_usbwin32 libusb-win32-bin-1.2.0.0 libusb-win32-bin-1.2.0.0.zip)
+SET(DEP_win7sdk win7-sdk win7-sdk.tar.gz)
+SET(DEP_zlib zlib-1.2.8 zlib-1.2.8.tar.gz)
+
+SET(DEP_ALL_PACKAGES 
+    DEP_boost
+    DEP_dlfcn
+    DEP_faac
+    DEP_faad
+    DEP_flac
+    DEP_glew
+    DEP_gpod 
+    DEP_gstbad
+    DEP_gstbase
+    DEP_gstgood
+    DEP_gst
+    DEP_gstugly
+    DEP_id3tag 
+    DEP_lame
+    DEP_lastfm
+    DEP_mad
+    DEP_mmswin32
+    DEP_ogg
+    DEP_oil
+    DEP_orc
+    DEP_plist 
+    DEP_pthreads
+    DEP_qjson
+    DEP_qt
+    DEP_quicktime
+    DEP_soup
+    DEP_speex
+    DEP_sqlite
+    DEP_taglib 
+    DEP_tls
+    DEP_usb
+    DEP_usbmuxd
+    DEP_vorbis
+    DEP_wpd
+    DEP_xml
+    DEP_qtsparkle 
+    DEP_pyqt
+    DEP_ofa
+    DEP_curl
+    DEP_fftw
+    DEP_archive 
+    DEP_protobuf
+    DEP_spotifywin32
+    DEP_glib
+    DEP_gettext
+    DEP_usbwin32 
+    DEP_gcrypt
+    DEP_gstlibav
+    DEP_mpcdec 
+    DEP_wavpack
+    DEP_gnutls
+    DEP_gpgerror
+    DEP_cdio 
+    DEP_opensslwin32
+    DEP_glibnetworking
+    DEP_echonest
+)
